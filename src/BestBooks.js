9650204a6850739145d99b06314521bb741df90a
MohdHanoti <mohadalhanoty@gmail.com>
2022-09-04 21:52:03 +0300

    version1

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import axios from 'axios';
+import Carousel from 'react-bootstrap/Carousel';
 
 class BestBooks extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       books: []
+
     }
   }
 
   /* TODO: Make a GET request to your API to fetch all the books from the database  */
+  componentDidMount = () => {
+    axios
+    .get(`http://localhost:3001/Books`)
+    .then(result =>{
+      console.log(result.data);
+      this.setState({
+        books : result.data
+      })
+    })
+    .catch(err=>{
+      console.log(err);
+    })
+    
+  }
 
   render() {
 
@@ -17,12 +34,34 @@ class BestBooks extends React.Component {
     return (
       <>
         <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
+        {this.state.books.map(item =>{
+          return(
+            
 
-        {this.state.books.length ? (
-          <p>Book Carousel coming soon</p>
-        ) : (
-          <h3>No Books Found :(</h3>
-        )}
+            // <div>
+            //   <h3>Book title : {item.title} </h3>
+            //   <p>description : {item.description}</p>
+            //   <p>status : {item.status}</p>
+            // </div>
+
+            <Carousel>
+      <Carousel.Item>
+        <img
+          className="d-block w-100"
+          src="data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBwgHBgkIBwgKCgkLDRYPDQwMDRsUFRAWIB0iIiAdHx8kKDQsJCYxJx8fLT0tMTU3Ojo6Iys/RD84QzQ5OjcBCgoKDQwNGg8PGjclHyU3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3Nzc3N//AABEIAH4AqAMBIgACEQEDEQH/xAAWAAEBAQAAAAAAAAAAAAAAAAAAAQf/xAAVEAEBAAAAAAAAAAAAAAAAAAAAAf/EABUBAQEAAAAAAAAAAAAAAAAAAAAB/8QAFBEBAAAAAAAAAAAAAAAAAAAAAP/aAAwDAQACEQMRAD8AxEEAAAAABQRQQEVFCKigAIKgKoAIAAlAAFQAUFQVBFAQABRFQRRFUAEABQBQRFAQAFQBVQEAAFEUAAEFRUAEBRFFAFAEABRABABFAAABFEUUEUEUAABABQARQABFAQAQAAAAAUURUUAAAAAAAUAEABRKKiIAKAAAABABQEUAAAABQQAABQQoIAAAIAAACgqKgACgKCAKgAACAUAAAAAAFQAFUARAFFAAf//Z"
+          alt="{item.title}"
+        />
+        <Carousel.Caption>
+          <h3>{item.title}</h3>
+          <p>{item.description}</p>
+          <p>status: {item.status}</p>
+        </Carousel.Caption>
+        </Carousel.Item>
+        </Carousel>
+          )
+        })}
+
+      
       </>
     )
   }
